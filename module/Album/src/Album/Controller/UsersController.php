84685c54895bfa2f994c5815c3b5457da767162d
cnpisit <pisit_lovlov@yahoo.com>
2013-06-04 17:58:12 +0700

    crud user

--- module/Album/src/Album/Controller/UsersController.php
+++ module/Album/src/Album/Controller/UsersController.php
@@ -31,54 +31,98 @@ class UsersController extends AbstractActionController
 
 
     public function indexAction() {
-<<<<<<< HEAD
-		$userdatas = array();
-
-            $users = $this->getEntityManager()->getRepository('Album\Entity\Users')->findAll();
-			$status = $this->getEntityManager()->getRepository('Album\Entity\Status')->findAll();
-
-
-//			var_dump($status);
-//		foreach ($users as $user)
-//		{
-//			$userdatas[] = $user;
-//		}
-
-//		return new ViewModel($userdatas);
-    var_dump($users);
+            $users = $this->getEntityManager()->getRepository('Album\Entity\User')->findAll();
+//			$status = $this->getEntityManager()->getRepository('Album\Entity\Status')->findAll();
+//        var_dump($users);
+		return new ViewModel( array (
+                    'you' => $users
+                    ));
     }
 	public function userproviderAction()
 	{
 
 	}
-	public function addAction(){
+	public function addAction()
+        {
+            $request = $this->getRequest();
+            
+            if($request->isPost())
+            {
+                $postData = (array) $request->getPost();
+                var_dump($postData);
+            }
+                if (isset($postData))
+                {
+                    $newPerson = new \Album\Entity\User();
 
-    }
-=======
-		
-        $users = $this->getEntityManager()->getRepository('Album\Entity\User')->findAll();
-        
-        return new ViewModel(array ('fuck' =>$users));
 
-    }
-	public function userproviderAction()
-	{
+                    $newPerson->setUsername($postData['username']);
+                    $newPerson->setDisplayName($postData['displayname']);
+                    $newPerson->setPassword($postData['password']);
+                    $newPerson->setEmail($postData['email']);
+                    $newPerson->setState($postData['state']);
 
-	}
-	public function addAction(){
+                    $em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+                    $em->persist($newPerson);
+                    $em->flush();
 
-    }
->>>>>>> 8aee110862b2be340c58d04a6626fe85f1b0b912
+                    $this->flashMessenger()->addMessage('<div class="alert alert-success">Success</div>');
+                    $this->redirect()->toRoute('users', array('controller' => 'users', 'action' => 'index'));
+                }
+
+        }
+
 
     public function deleteAction(){
+        $id = $this->params('id');
 
+		$user = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default')->find('Album\Entity\User', $id);
+		if(null == $user){
+			$this->redirect()->toRoute('users', array('controller' => 'users', 'action' => 'index'));
+		}
+
+		$em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+		$em->remove($user);
+		$em->flush();
+
+		$this->flashMessenger()->addMessage('<div class="alert alert-success">Success</div>');
+		return $this->redirect()->toRoute('users', array('controller' => 'users', 'action' => 'index'));
     }
-<<<<<<< HEAD
 
-    public function editAction(){
-
-=======
     public function updateAction(){
+       $request = $this->getRequest();
+        
+        if($request->isPost())
+        {
+            $postData = (array) $request->getPost();
+//          
+            $id = $postData['userId'];
+            $user = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default')->find('Album\Entity\User', $id);
+            
+            if(null == $user)
+            {
+                $this->redirect()->toRoute('users', array('controller ' => 'Users', 'action' => 'index'));
+                
+            }
+            if(isset($postData))
+            {
+//                var_dump($user);
+
+                $user->setUsername($postData['username']);
+                $user->setEmail($postData['email']);
+                $user->setDisplayName($postData['displayname']);
+                $user->setPassword($postData['password']);
+                $user->setState($postData['state']);
+
+                $em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+                $em->persist($user);
+                $em->flush();
+
+                $this->flashMessenger()->addMessage('<div class="alert alert-success">Success</div>');
+                $this->redirect()->toRoute('users', array('controller' => 'users', 'action' => 'index'));
+            }
+            
+        }
 
     }
 
@@ -96,7 +140,7 @@ class UsersController extends AbstractActionController
             'user' => $shop,
             'submitText' => 'Update',
          ));
->>>>>>> 8aee110862b2be340c58d04a6626fe85f1b0b912
+
     }
 }
 
